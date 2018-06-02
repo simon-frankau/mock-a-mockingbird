aa768207eecf7c0652e632c233d794ef3ccf1feb
Simon Frankau <sgf@arbitrary.name>
2018-06-02 22:50:54 +0100

    Data structure tidy-up

--- BirdsGalore.hs
+++ BirdsGalore.hs
@@ -8,13 +8,11 @@ import Data.Maybe(fromJust)
 ------------------------------------------------------------------------
 -- Expression type and pretty-printer
 
+data Combinator = W | K | M | I | C | T deriving (Show, Eq, Ord)
+
+
 data Expr = Ap Expr Expr
-          | W
-          | K
-          | M
-          | I
-          | C
-          | T
+          | Co Combinator
           | Var Integer
             deriving (Eq, Ord)
 
@@ -24,33 +22,28 @@ instance Show Expr where
 show' needBracket (Ap e1 e2) =
   let e = show' False e1 ++ " " ++ show' True e2
   in if needBracket then "(" ++ e ++ ")" else e
-show' _ W = "W"
-show' _ K = "K"
-show' _ M = "M"
-show' _ I = "I"
-show' _ C = "C"
-show' _ T = "T"
+show' _ (Co  c) = show c
 show' _ (Var i) = "X" ++ show i
 
 ------------------------------------------------------------------------
 -- Build and reduce expressions
 
 -- Build expression with limited leaves.
-exprOfSize :: [Expr] -> Integer -> [Expr]
+exprOfSize :: [Combinator] -> Integer -> [Expr]
 exprOfSize xs = aux where
   aux 0 = []
-  aux 1 = xs
+  aux 1 = map Co xs
   aux n = concatMap doAp [1..n - 1] where
     doAp i = Ap <$> aux i <*> aux (n - i)
 
 step :: Expr -> Maybe Expr
 -- Actual combinator cases
-step     (Ap (Ap W x) y)    = Just $ Ap (Ap x y) y
-step     (Ap (Ap K x) y)    = Just $ x
-step         (Ap M x)       = Just $ Ap x x
-step         (Ap I x)       = Just $ x
-step (Ap (Ap (Ap C x) y) z) = Just $ Ap (Ap x z) y
-step     (Ap (Ap T x) y)    = Just $ Ap y x
+step     (Ap (Ap (Co W) x) y)    = Just $ Ap (Ap x y) y
+step     (Ap (Ap (Co K) x) y)    = Just $ x
+step         (Ap (Co M) x)       = Just $ Ap x x
+step         (Ap (Co I) x)       = Just $ x
+step (Ap (Ap (Ap (Co C) x) y) z) = Just $ Ap (Ap x z) y
+step     (Ap (Ap (Co T) x) y)    = Just $ Ap y x
 -- No match? Then try digging down the left.
 step (Ap l r) = flip Ap r <$> step l
 -- Still didn't work?
@@ -72,7 +65,7 @@ maxSteps = 10
 
 isOnlyVars (Ap x y) = isOnlyVars x && isOnlyVars y
 isOnlyVars (Var _)  = True
-isOnlyVars _        = False
+isOnlyVars (Co  _)  = False
 
 applyNVars :: Expr -> Integer -> Expr
 applyNVars e = aux where
@@ -87,15 +80,16 @@ findNVars e = fst $ head $
               [0..]
 
 -- Find a way to build a combinator using a different set of combinators
-findExpr :: Expr -> [Expr] -> Expr
+findExpr :: Combinator -> [Combinator] -> Expr
 findExpr target avail = head $ filter isTarget $ allExprsOf avail where
-  numVars = findNVars target
+  targetExpr = Co target
+  numVars = findNVars targetExpr
   applyReduce = boundSteps maxSteps . flip applyNVars numVars
-  targetWithVars = applyReduce target
+  targetWithVars = applyReduce targetExpr
   isTarget candidate = applyReduce candidate == targetWithVars
 
 -- Find an pretty-print a solution
-solve :: Integer -> Expr -> [Expr] -> IO ()
+solve :: Integer -> Combinator -> [Combinator] -> IO ()
 solve problemNum target avail = do
   let solution = findExpr target avail
   putStrLn $ "Problem " ++ show problemNum ++ ": " ++
