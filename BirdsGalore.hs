bb851078529b0e6e100cec22accacda1f63ce954
Simon Frankau <sgf@arbitrary.name>
2018-06-02 23:38:51 +0100

    Automated problems 13-30, mostly

--- BirdsGalore.hs
+++ BirdsGalore.hs
@@ -8,8 +8,8 @@ import Data.Maybe(fromJust)
 ------------------------------------------------------------------------
 -- Expression type and pretty-printer
 
-data Combinator = W | K | M | I | C | T deriving (Show, Eq, Ord)
-
+data Combinator = W | K | M | I | C | T | B | R | F | E | V
+                  deriving (Show, Eq, Ord)
 
 data Expr = Ap Expr Expr
           | Co Combinator
@@ -38,12 +38,17 @@ exprOfSize xs = aux where
 
 step :: Expr -> Maybe Expr
 -- Actual combinator cases
-step     (Ap (Ap (Co W) x) y)    = Just $ Ap (Ap x y) y
-step     (Ap (Ap (Co K) x) y)    = Just $ x
-step         (Ap (Co M) x)       = Just $ Ap x x
-step         (Ap (Co I) x)       = Just $ x
-step (Ap (Ap (Ap (Co C) x) y) z) = Just $ Ap (Ap x z) y
-step     (Ap (Ap (Co T) x) y)    = Just $ Ap y x
+step             (Ap (Ap (Co W) x) y)          = Just $ Ap (Ap x y) y
+step             (Ap (Ap (Co K) x) y)          = Just $ x
+step                 (Ap (Co M) x)             = Just $ Ap x x
+step                 (Ap (Co I) x)             = Just $ x
+step         (Ap (Ap (Ap (Co C) x) y) z)       = Just $ Ap (Ap x z) y
+step             (Ap (Ap (Co T) x) y)          = Just $ Ap y x
+step         (Ap (Ap (Ap (Co B) x) y) z)       = Just $ Ap x (Ap y z)
+step         (Ap (Ap (Ap (Co R) x) y) z)       = Just $ Ap (Ap y z) x
+step         (Ap (Ap (Ap (Co F) x) y) z)       = Just $ Ap (Ap z y) x
+step (Ap (Ap (Ap (Ap (Ap (Co E) x) y) z) w) v) = Just $ Ap (Ap x y) (Ap (Ap z w) v)
+step         (Ap (Ap (Ap (Co V) x) y) z)       = Just $ Ap (Ap z x) y
 -- No match? Then try digging down the left.
 step (Ap l r) = flip Ap r <$> step l
 -- Still didn't work?
@@ -101,3 +106,22 @@ main = do
   solve 15 I [W, K]
   solve 16 I [C, K]
   solve 17 T [C, I]
+  solve 20 R [B, T]
+  solve 21 C [R]
+  solve 21 C [B, T]
+  solve 23 R [C]
+  solve 24 F [B, R, C]
+  solve 25 F [T, E]
+  solve 26 F [B, T]
+  solve 27 V [C, F]
+  -- Too slow - search space is too big.
+  -- solve 27 V [B, T]
+  -- Instead, we know V = C F
+  let c = findExpr C [B, T]
+      f = findExpr F [B, T]
+      v = Ap c f
+  putStrLn $ "Problem 27: V = " ++
+             show (fromJust $ boundSteps maxSteps v)
+  solve 28 V [F, R]
+  solve 29 F [C, V]
+  solve 30 I [R, K]
--- Chapter11.md
+++ Chapter11.md
@@ -169,3 +169,58 @@ X (X X) (X X) -> B B (B B)
 X (X X X) (X X X) -> B (B B B) (B (B B B))
 ```
 
+## Some Other Birds
+
+### Problems 13-30
+
+With the exceptions of the problems below, they're solved by
+BirdsGalore.hs:
+
+```
+Problem 13: M = W (W K)
+Problem 14: M = W I
+Problem 15: I = W K
+Problem 16: I = C K C
+Problem 17: T = C I
+Problem 20: R = B B T
+Problem 21: C = R R R
+Problem 21: C = B (T (B B T)) (B B T)
+Problem 23: R = C C
+Problem 24: F = B C R
+Problem 25: F = E T T E T
+Problem 26: F = B (T T) (B B (B B T))
+Problem 27: V = C F
+Problem 27: V = B (T (B (T T) (B B (B B T)))) (B B T)
+Problem 28: V = R F R
+Problem 29: F = C V
+Problem 30: I = R R K
+```
+
+### Problem 18
+
+Let's say all birds are fond of some bird x, so that for all y, y x =
+x. T y x = x y, but (T y) is also fond of x, so x y = x = y x. x
+commutes with everything.
+
+...
+
+Then it turns out I misinterpreted the question. They're not all fond
+of the same bird.
+
+T is fond of a bird A: T A = A. But T A x = A x and T A x = x A, so A
+x = x A. This bird is the bird we're looking for. D'oh.
+
+### Problem 19
+
+The fixed point of T is `M (T . M) = M (B T M)`
+
+### Problem 22
+
+```
+C x = R R R x = R x R
+    = B B T x R = B (T x) R
+```
+
+## Some Relatives
+
+TODO
