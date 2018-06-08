a466e1860339a6a19ca9ae93ca6af8f300bae388
Simon Frankau <sgf@arbitrary.name>
2018-06-08 21:13:24 +0100

    Make the solvable things into a type class

--- BirdsGalore.hs
+++ BirdsGalore.hs
@@ -117,32 +117,33 @@ findNVars e = fst $ head $
               map (id &&& (fromJust . boundSteps maxSteps . applyNVars e)) $
               [0..]
 
--- Find a way to build a combinator using a different set of combinators
-findExpr :: Combinator -> [Combinator] -> Expr
-findExpr target avail = head $ filter isTarget $ allExprsOf avail where
-  targetExpr = Co target
-  numVars = findNVars targetExpr
-  applyReduce = boundSteps maxSteps . flip applyNVars numVars
-  targetWithVars = applyReduce targetExpr
-  isTarget candidate = applyReduce candidate == targetWithVars
+applyReduce :: Expr -> Integer -> Maybe Expr
+applyReduce e i = boundSteps maxSteps $ applyNVars e i
 
--- Find an expression that permutes the supplied parameters as needed
-findExpr' :: Expr -> [Combinator] -> Expr
-findExpr' target avail = head $ filter isTarget $ allExprsOf avail where
-  numVars = maxVar target
-  applyReduce = boundSteps maxSteps . flip applyNVars numVars
-  isTarget candidate = applyReduce candidate == Just target
+class GetTarget a where
+  -- Returns the reduced expression and the number of variables needed
+  getTarget :: a -> (Maybe Expr, Integer)
+
+instance GetTarget Combinator where
+  getTarget target = (targetWithVars, numVars) where
+    targetExpr = Co target
+    numVars = findNVars targetExpr
+    targetWithVars = applyReduce targetExpr numVars
+
+instance GetTarget Expr where
+  getTarget target = (Just target, maxVar target) where
+
+-- Find a way to build a combinator using a different set of combinators
+findExpr :: GetTarget a => a -> [Combinator] -> Expr
+findExpr target avail = head $ filter isTarget $ allExprsOf avail where
+  (targetWithVars, nVars) = getTarget target
+  isTarget candidate = applyReduce candidate nVars == targetWithVars
 
 -- Find an pretty-print a solution
-solve :: Integer -> Combinator -> [Combinator] -> IO ()
+solve :: (GetTarget a, Show a) =>
+         Integer -> a -> [Combinator] -> IO ()
 solve problemNum target avail = do
   let solution = findExpr target avail
-  putStrLn $ "Problem " ++ show problemNum ++ ": " ++
-             show target ++ " = " ++ show solution
-
-solve' :: Integer -> Expr -> [Combinator] -> IO ()
-solve' problemNum target avail = do
-  let solution = findExpr' target avail
   putStrLn $ "Problem " ++ show problemNum ++ ": " ++
              show target ++ " = " ++ show solution
 
@@ -174,22 +175,22 @@ main = do
   -- For the ones where we're not reusing the combinators, don't
   -- bother adding them as actual combinators.
   let (x, y, z, w, v) = (Var 1, Var 2, Var 3, Var 4, Var 5)
-  solve' 31 (x # y # w # z) [B, C]
-  solve' 32 (x # z # w # y) [B, C]
-  solve' 33 (x # w # z # y) [B, C]
-  solve' 34 (x # w # y # z) [B, C] -- Typo in the book
-  solve' 35 (x # y # z # v # w) [B, C]
-  solve' 35 (x # y # w # v # z) [B, C]
-  solve' 35 (x # y # v # w # z) [B, C]
-  solve' 35 (x # y # v # z # w) [B, C]
+  solve 31 (x # y # w # z) [B, C]
+  solve 32 (x # z # w # y) [B, C]
+  solve 33 (x # w # z # y) [B, C]
+  solve 34 (x # w # y # z) [B, C] -- Typo in the book
+  solve 35 (x # y # z # v # w) [B, C]
+  solve 35 (x # y # w # v # z) [B, C]
+  solve 35 (x # y # v # w # z) [B, C]
+  solve 35 (x # y # v # z # w) [B, C]
   solve 36 V [CStar, T]
-  solve' 37 (y # (x # z)) [B, C]
+  solve 37 (y # (x # z)) [B, C]
   -- TODO! Too slow...
-  -- solve' 38 (x # (z # y)) [B, T]
-  solve' 39 (y # (z # x)) [B, T]
-  solve' 41 (z # (x # y)) [B, T]
-  solve' 42 (z # (y # x)) [B, T]
+  -- solve 38 (x # (z # y)) [B, T]
+  solve 39 (y # (z # x)) [B, T]
+  solve 41 (z # (x # y)) [B, T]
+  solve 42 (z # (y # x)) [B, T]
   solve 45 B [Q, T]
   solve 46 C [Q, T]
   -- TODO: Too slow:
-  -- solve' 47 (x # w # (y # z)) [B, T]
+  -- solve 47 (x # w # (y # z)) [B, T]
